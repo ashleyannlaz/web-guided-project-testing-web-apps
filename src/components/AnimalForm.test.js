9917be6c4c6ced8c52451ce80f305cc6cb3638b5
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-09 18:18:47 -0700

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -31,3 +31,29 @@ test("User can add a new animal by filling out the form", async () => {
     expect(invisibleAnimal).toEqual(null);
     expect(invisibleAnimal).toBeFalsy();
 });
+
+
+test("User can add multiple animals", () => {
+    // Arrange: render & grab the elements we need
+    render(<AnimalForm />);
+
+    const speciesInput = screen.getByLabelText(/species/i);
+    const ageInput = screen.getByLabelText(/age/i);
+    const notesInput = screen.getByLabelText(/notes/i);
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+
+    // Act: fill out the form and click the button (simulating user behavior with userEvent)
+    userEvent.type(speciesInput, "Deer");
+    userEvent.type(ageInput, "98");
+    userEvent.type(notesInput, "I love the band 98 Degrees");
+    userEvent.click(submitButton);
+
+    // Assert
+    const newAnimal = screen.getByText(/deer/i); // implicit assertion here
+    expect(newAnimal).toBeInTheDocument();
+
+    // Example of asserting that something isn't in the DOM:
+    const invisibleAnimal = screen.queryByText(/llama/i);
+    expect(invisibleAnimal).toEqual(null);
+    expect(invisibleAnimal).toBeFalsy();
+});
