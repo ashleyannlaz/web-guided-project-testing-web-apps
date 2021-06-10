c3472636a95bf2265fb93e0695fa77804e392ba0
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-09 17:06:20 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from "react";
 import App from "./App";
 
-test("App component", () => {
+// Don't worry about being DRY when writing tests
+// Better to make them extra clear!! AHA - Avoid Hasty Abstractions
+// For example, if you have the same setup (Arrange) steps for a few different tests, you might think to refactor those into a helper function. That would be good logic for application code, but not here! We want to keep each test as independent and self-contained as possible. We're going for coverage and rigor here, not DRYness.
+
+test("App component renders", () => {
     // do test things:
     // Arrange
 
