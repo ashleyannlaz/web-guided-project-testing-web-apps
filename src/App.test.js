c4cb8d17a25c5b2e042c17732e5e3f0efb9effe9
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-09 17:15:34 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -7,11 +7,13 @@ import App from "./App";
 // For example, if you have the same setup (Arrange) steps for a few different tests, you might think to refactor those into a helper function. That would be good logic for application code, but not here! We want to keep each test as independent and self-contained as possible. We're going for coverage and rigor here, not DRYness.
 
 test("App component renders", () => {
-    // do test things:
     // Arrange
-    const stuff = render(<App />);
-    console.log(stuff);
-    // Act
+    render(<App />);
 
-    // Assert
-})
+    // (No Act needed here)
+
+    // Assert: we already have the implicit assertion that the component renders above. If that render command errors out in any way, this test will fail
+});
+
+
+
